f0974df4db651ef2702b9f4ba6b9cf75f0150fe0
Tom Cockram <tomcockram6@gmail.com>
2021-10-29 15:17:37 +0100

    feat: retrieve weather info and display on page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'weather-app';
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+
+  dummyData = {};
+
+  ngOnInit() {
+    this.http
+      .get(
+        'http://api.weatherstack.com/current?access_key=0e50280b1799d67ffb967d013b015c5f&query=horsham'
+      )
+      .subscribe((testData) => {
+        this.dummyData = testData;
+      });
+  }
 }
